08862534c4740a25a5e85a35433f2cf18ad0d37f
Mukund <mukundchouhan220@gmail.com>
2017-07-02 17:11:21 +0530

    changed package name

---
+++ frm-main/frm-utils/src/main/java/com/mukund/frm/utils/App.java
@@ -0,0 +1,13 @@
+package com.mukund.frm.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
